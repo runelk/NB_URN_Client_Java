92544eddd5442699509045149c3af1e0ab2610d5
Rune Lain Knudsen <rune.knudsen@uib.no>
2014-05-28 08:22:47 +0200

    More functionality and helper methods in the NbUrnClient class.

--- src/main/java/no/clarino/pid/nb/urn/NbUrnClient.java
+++ src/main/java/no/clarino/pid/nb/urn/NbUrnClient.java
@@ -1,17 +1,53 @@
 package no.clarino.pid.nb.urn;
 
 import no.clarino.pid.nb.urn.wsdl.*;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
 
 /**
  * Created by rkn083 on 19.05.14.
  */
 public class NbUrnClient {
+    private Map <String, String> config;
     private IdService client;
     private String ssoToken;
 
-    public NbUrnClient() {
+    public NbUrnClient(String config) throws NbUrnException {
         this.client = new IdService_Service().getPort(IdService.class);
+        this.config = loadConfig(config);
+    }
 
+    public Map<String, String> getConfig() {
+        return config;
+    }
+
+    public void setConfig(Map<String, String> config) {
+        this.config = config;
+    }
+
+    private Map loadConfig(String filename) throws NbUrnException {
+        File f = new File(filename);
+        try {
+            FileInputStream fileInputStream = new FileInputStream(f);
+            Yaml yaml = new Yaml();
+            Map m = (Map <String, String>) ((Map) ((Map) yaml.load(fileInputStream)).get("config")).get("urn");
+            return m;
+        } catch (FileNotFoundException e) {
+            throw new NbUrnException(e);
+        }
+    }
+
+    public NbUrnClient(String config, String username, String password) throws NbUrnException {
+        this.client = new IdService_Service().getPort(IdService.class);
+        Yaml yaml = new Yaml();
+        this.config = loadConfig(config);
+        this.config.put("username", username);
+        this.config.put("password", password);
+        System.out.println(this.config.toString());
     }
 
     public String getSsoToken() {
@@ -73,17 +109,29 @@ public class NbUrnClient {
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
-    public String login(String username, String password) throws NbUrnException {
-        //throw new UnsupportedOperationException("Not implemented yet");
+    public String login() throws NbUrnException {
         try {
-            return this.client.login(username, password);
+            this.ssoToken = this.client.login(this.config.get("username"), this.config.get("password"));
+            return this.ssoToken;
+        } catch (FailedLoginException_Exception e) {
+            throw new NbUrnException(e);
+        }
+    }
+
+    public String login(String username, String password) throws NbUrnException {
+        try {
+            this.ssoToken = this.client.login(username, password);
+            return this.ssoToken;
         } catch (FailedLoginException_Exception e) {
             throw new NbUrnException(e);
         }
     }
 
     public void logout() {
-
+        if (this.ssoToken != null) {
+            this.client.logout(this.ssoToken);
+            this.ssoToken = null;
+        }
     }
 
     public void registerUrn(String urn, String url) {
@@ -105,4 +153,5 @@ public class NbUrnClient {
     public void setDefaultUrl(String urn, String url) {
         throw new UnsupportedOperationException("Not implemented yet");
     }
+
 }
